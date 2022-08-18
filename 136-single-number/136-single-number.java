8022490709e459588af0533bce96f82d76e5dafe
YoufuYAN <arnoldyyf@gmail.com>
2022-08-18 19:32:31 -0400

    Time: 2 ms (64.56%), Space: 50.1 MB (79.23%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -6,4 +6,7 @@ class Solution {
         }
         return res;
     }
+
+//Time complexity : O(n). We only iterate through nums, so the time complexity is the number of elements in nums.
+//Space complexity : O(1).
 }
