f67f839a33d060698d5af081b1bcea6a731b9d05
Youfu YAN <arnoldyyf@gmail.com>
2023-08-14 20:43:02 -0400

    Time: 0 ms (100.00%), Space: 41 MB (53.05%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,29 +1,17 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String res = "";
-        int len1 = word1.length(), len2= word2.length();
-        if (len1 == 0) {
-            return word2;
-        } else if (len2 == 0) {
-            return word1;
+        StringBuilder res = new StringBuilder();
+        int m = word1.length(), n = word2.length();
+        int i = 0, j = 0;
+        while (i < m || j < n) {
+            if (i < m) {
+                res.append(word1.charAt(i++));
+            }
+            if (j < n) {
+                res.append(word2.charAt(j++));
+            }
+            
         }
-        if (len1 > len2) {
-            for (int i = 0 ; i < len2; i++) {
-                res += word1.charAt(i);
-                res += word2.charAt(i);
-            }
-            for (int j = len2; j < len1; j++) {
-                res += word1.charAt(j);
-            }
-        } else {
-             for (int i = 0 ; i < len1; i++) {
-                res += word1.charAt(i);
-                res += word2.charAt(i);
-            }
-            for (int j = len1; j < len2; j++) {
-                res += word2.charAt(j);
-            }
-        }
-        return res;
+        return res.toString();
     }
 }
