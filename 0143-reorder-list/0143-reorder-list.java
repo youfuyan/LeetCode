66299d6d3c0e8acf484470fc9b8d786e675e054d
Youfu YAN <arnoldyyf@gmail.com>
2023-12-17 14:09:32 -0600

    Time: 4 ms (18.19%), Space: 46.4 MB (26.72%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,34 +10,23 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head.next;
-        //find the middle of linked list
-        while(fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        Stack<ListNode> stk = new Stack<>();
+        ListNode p = head;
+        while (p != null) {
+            stk.push(p);
+            p = p.next;
         }
-        
-        //Reverse the second half of the list
-        ListNode second = slow.next;
-        ListNode prev = slow.next = null;
-        while (second != null) {
-            ListNode temp = second.next;
-            second.next = prev;
-            prev = second;
-            second = temp;
+        p = head;
+        while(p != null) {
+            ListNode lastNode = stk.pop();
+            ListNode next = p.next;
+            if (lastNode == next || lastNode.next == next) {
+                lastNode.next = null;
+                break;
+            }
+            p.next = lastNode;
+            lastNode.next = next;
+            p = next;
         }
-        
-        //Re-assign the pointers to match the pattern
-        ListNode first = head;
-        second = prev;
-        while (second != null) {
-            ListNode temp1 = first.next;
-            ListNode temp2 = second.next;
-            first.next = second;
-            second.next = temp1;
-            first = temp1;
-            second = temp2;
-        }
-     }
+    }
 }
