75fa66354b80e81227d0e962f07a6b7af0e1126f
Youfu YAN <arnoldyyf@gmail.com>
2023-12-16 20:36:19 -0600

    Time: 1 ms (100.00%), Space: 53.8 MB (19.26%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n];
+        prefix[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] * nums[i];
+        }
+        int[] suffix = new int[n];
+        suffix[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * nums[i];
+        }
+        int[] res = new int[n];
+        res[0] = suffix[1];
+        res[n - 1] = prefix[n - 2];
+        for (int i = 1; i < n - 1; i++) {
+            res[i] = prefix[i - 1] * suffix[i + 1];
+        }
+        return res;
+    }
+}
