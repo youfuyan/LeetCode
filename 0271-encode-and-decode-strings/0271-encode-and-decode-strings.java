43860130e51e2e4b3529964bb92c7aeb261f90ad
Youfu YAN <arnoldyyf@gmail.com>
2023-12-16 21:01:19 -0600

    Time: 4 ms (96.79%), Space: 44.9 MB (25.00%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -1,17 +1,25 @@
 public class Codec {
 
-    public String encode(List<String> strs) {
-        return strs.stream()
-                   .map(s -> s.replace("#", "##") + " # ")
-                   .collect(Collectors.joining());
+   public String encode(List<String> strs) {
+        StringBuilder encodedString = new StringBuilder();
+        for (String str : strs) {
+            encodedString.append(str.length()).append("#").append(str);
+        }
+        return encodedString.toString();
     }
 
-    public List<String> decode(String s) {
-        List strs = Stream.of(s.split(" # ", -1))
-                          .map(t -> t.replace("##", "#"))
-                          .collect(Collectors.toList());
-        strs.remove(strs.size() - 1);
-        return strs;
+    public List<String> decode(String str) {
+        List<String> list = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '#') j++;
+
+            int length = Integer.valueOf(str.substring(i, j));
+            i = j + 1 + length;
+            list.add(str.substring(j + 1, i));
+        }
+        return list;
     }
 }
 
