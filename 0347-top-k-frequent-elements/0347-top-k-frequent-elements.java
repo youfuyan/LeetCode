375c84593134f4ce3a32aff855fa3803cd772a97
Youfu YAN <arnoldyyf@gmail.com>
2023-12-16 19:26:14 -0600

    Time: 12 ms (81.65%), Space: 47.6 MB (50.14%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,23 +1,57 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> valToFreq = new HashMap<>();
-        for (int v : nums) {
-            valToFreq.put(v, valToFreq.getOrDefault(v, 0) + 1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        };
+        int size = map.size();
+        int[] keys = new int[size];
+        int i = 0;
+        for (int key: map.keySet()) {
+            keys[i++] = key;
         }
-        
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> {return a.getValue().compareTo(b.getValue());});
-        for (Map.Entry<Integer, Integer> e : valToFreq.entrySet()) {
-            pq.offer(e);
-            if (pq.size() > k) {
-                pq.poll();
-            }
+        select(keys, map, 0, size - 1, size - k);
+        return Arrays.copyOfRange(keys, size - k, size); 
+    }
+    
+    private void select(
+        int[] keys,
+        Map<Integer, Integer> map,
+        int left,
+        int right,
+        int kSmallest
+    ) {
+        while (left != right) {
+            int pivot = partition(keys, map, left, right, (left + right) / 2);
+
+            if (kSmallest == pivot) return;
+
+            if (kSmallest < pivot) right = pivot - 1; else left = pivot + 1;
         }
-        
-        int[] res = new int[k];
-        for (int i = k - 1; i >=0 ; i--) {
-            res[i] = pq.poll().getKey();
+    }
+
+     private int partition(
+        int[] keys,
+        Map<Integer, Integer> map,
+        int left,
+        int right,
+        int pivot
+    ) {
+        int pivotValue = map.get(keys[pivot]);
+        swap(keys, pivot, right);
+        int index = left;
+
+        for (int i = left; i <= right; i++) if (map.get(keys[i]) < pivotValue) {
+            swap(keys, i, index);
+            index++;
         }
-        
-        return res;
-    } 
+        swap(keys, right, index);
+        return index;
+    }
+    
+    void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
